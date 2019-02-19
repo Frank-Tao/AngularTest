835b281a1b8aba417b0224e61a136a7ede8655c5
Frank Tao <frankt@lantrak.com.au>
2019-02-19 12:32:37 +1100

    Karma Test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        HttpClientModule,
+        RouterTestingModule        
       ],
       declarations: [
         AppComponent
@@ -26,10 +28,10 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('AngularTest');
   });
 
-  it('should render title in a h1 tag', () => {
+  it('should render title in a h2 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to AngularTest!');
+    expect(compiled.querySelector('h2').textContent).toContain('Welcome to AngularTest');
   });
 });
--- src/app/cat.service.spec.ts
+++ src/app/cat.service.spec.ts
@@ -1,9 +1,11 @@
 import { TestBed } from '@angular/core/testing';
-
 import { CatService } from './cat.service';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('CatService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({imports: [
+    HttpClientModule      
+  ]})); 
 
   it('should be created', () => {
     const service: CatService = TestBed.get(CatService);
